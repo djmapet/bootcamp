2f00e3ee09390e33e63e7000746ff5a687dcd7b4
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-17 22:58:22 +0900

    for文の修正その２

--- practice4/practice4.py
+++ practice4/practice4.py
@@ -3,10 +3,11 @@ with open("oscar.csv","r") as open_text:
     reader = csv.reader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
                         skipinitialspace=True)
     header = next(reader)
-    print(header)
 
     for row in reader:
-        print(row[1] =="1991" ,row[1], row[3])
+        if row[1] == "1991":
+            print(row[3])
+
 
 
 
