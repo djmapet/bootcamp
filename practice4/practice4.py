b5c2e8e2f9ebf53ae5f74fb71cd8c53bdb7e32a9
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-17 21:08:32 +0900

    if文追加して1991年は誰が受賞したかのプログラムを作った

--- practice4/practice4.py
+++ practice4/practice4.py
@@ -2,11 +2,15 @@ import csv
 with open("oscar.csv","r") as open_text:
     reader = csv.reader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
                         skipinitialspace=True)
-    reader = csv.DictReader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
-                            skipinitialspace=True)
+    header = next(reader)
+    print(header)
 
-    for line in reader:
-        reader = [row for row in line]
-        print(line)
+    for row in reader:
+        print(row)
+
+row_oscar = int(input('1991年と入力してください'))
+if row_oscar == 1991:
+    print("{}年はKathy Batesがオスカー賞を受賞しました".format(row_oscar))
+
 
 
