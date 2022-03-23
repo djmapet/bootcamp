10a4e96b89c4d1dca797d8644e543ccf00994622
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-23 22:02:54 +0900

    女優の名前リストを出すことに成功(2回目)

--- practice4/practice4.py
+++ practice4/practice4.py
@@ -3,6 +3,7 @@ import collections
 import csv
 u30 = 0
 u30_list = list()
+count = 0
 with open("oscar.csv","r") as open_text:
     reader = csv.reader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
                         skipinitialspace=True)
@@ -18,10 +19,19 @@ with open("oscar.csv","r") as open_text:
 
 
 
+
+
+
     print("1991年のオスカー女優は%sです"
           %(oscar_1991))
-
     print("30歳以下のオスカー女優は%s名です"
           %(u30))
-    print("30歳以下のオスカー女優は%sです"
-          %(u30_list))
+
+    print("30歳以下のオスカー女優は以下の通りです")
+    for l in u30_list:
+        count +=1
+        names = l
+
+        print("{0:d}".format(count),"{0:s}".format(names))
+
+
