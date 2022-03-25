b64cd9f7b8d9f4ec62601d4e5227bdd20efbd28d
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-25 11:46:34 +0900

    平均の計算式を書いてみた

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -1,6 +1,7 @@
 #coding: UTF-8
 import csv
 u20_woman = list()
+ave_count = list()
 with open("titanic.csv","r") as open_text:
     reader = csv.reader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
                         skipinitialspace=True)
@@ -11,9 +12,18 @@ with open("titanic.csv","r") as open_text:
             n = (titanic[4],titanic[2])
             u20_woman.append(n)
 
+        if titanic[0] == "1" and titanic[3] == "female""male":
+            ave = sum(titanic[4])/len(titanic[4])
+            print("%d" % (ave))
+
 
     for l in u20_woman:
         age = l[0]
         name = l[1]
         print("%s,%s" % (age,name))
+
+
+
+
+
 
