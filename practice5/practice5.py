cc7c8c16f9c905fb7d363117048ee674c33044cb
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-29 15:56:28 +0900

    print文修正してcommit

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -2,8 +2,10 @@
 import csv
 u20_woman = list()
 ave = list()
-total_age = 0
-count = 0
+total_female_age = 0
+total_male_age = 0
+female_count = 0
+male_count = 0
 
 with open("titanic.csv","r") as open_text:
     reader = csv.reader(open_text,delimiter="\t",doublequote=True, lineterminator="\r\n",quotechar='"',
@@ -15,24 +17,32 @@ with open("titanic.csv","r") as open_text:
             n = (titanic[4],titanic[2])
             u20_woman.append(n)
 
-        if titanic[0] == "1":
+        if titanic[0] == "1" and titanic[3] == "female":
             try:
-                age = int(titanic[4])
+                female_age = int(titanic[4])
             except ValueError:
                 continue
 
-            total_age += age
-            count += 1
-            ave = total_age/count
+            total_female_age += female_age
+            female_count += 1
 
+        if titanic[0] == '1' and titanic[3] == "male":
+            try:
+                male_age = int(titanic[4])
+            except ValueError:
+                continue
 
+            total_male_age += male_age
+            male_count +=1
 
-
-
+female_ave = total_female_age/female_count
+male_ave = total_male_age/male_count
 for l in u20_woman:
     age = l[0]
     name = l[1]
     print("%s,%s" % (age, name))
 
-print('生存者の平均年齢は','{:.1f}'.format(ave),'歳です' )
+
+print('女性生存者の平均年齢は%d歳です' %female_ave )
+print('男性生存者の平均年齢は%d歳です' %male_ave )
 
