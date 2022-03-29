f415654c518675424483c5e7eb8669904a4b602a
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-03-29 20:39:50 +0900

    ifの中にifを入れた

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -23,14 +23,14 @@ with open("titanic.csv","r") as open_text:
             except ValueError:
                 continue
 
-        if titanic[0] == "1" and titanic[3] =="female":
+            if titanic[0] == "1" and titanic[3] =="female":
 
-            total_female_age += age
-            female_count += 1
+                total_female_age += age
+                female_count += 1
 
-        if titanic[0] == "1" and titanic[3] == "male":
-            total_male_age += age
-            male_count += 1
+            if titanic[0] == "1" and titanic[3] == "male":
+                total_male_age += age
+                male_count += 1
 
 female_ave = total_female_age/female_count
 male_ave = total_male_age/male_count
