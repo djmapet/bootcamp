d9b8bfc9b0284442b7a89cfae093bcce010c6656
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-01 16:13:36 +0900

    print文を修正したので2回目をcommit

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -25,7 +25,7 @@ with open("titanic.csv","r") as open_text:
             try:
                 age = int(titanic[4])
                 name = (titanic[2])
-                age_name = age,name
+                age_name = name,age
                 age_list.append(age_name)
 
             except ValueError:
@@ -57,13 +57,10 @@ for l in u20_woman:
 
 
 
-
-
-
 print('女性生存者の平均年齢は%d歳です' %female_ave )
 print('男性生存者の平均年齢は%d歳です' %male_ave )
 print('生存者の最高年齢は%d歳です'%max_age)
 print('生存者の最小年齢は%d歳です'%minimu_age)
 
-print('生存者の最高年齢は%d歳で%sです'%max(age_list))
-print('生存者の最高年齢は%d歳で%sです'%min(age_list))
+print('生存者の最高年齢は%sの%d歳です'%max(age_list))
+print('生存者の最小年齢は%sの%d歳です'%min(age_list))
