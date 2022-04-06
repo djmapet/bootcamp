af71f6598246a3fc43c6bce2f2ba21a0618c6d5a
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-06 17:13:25 +0900

    答えが出たので一旦commit

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -23,14 +23,14 @@ with open("titanic.csv","r") as open_text:
 
         if titanic[0] == "1":
             try:
-                name = (titanic[2])
                 age = int(titanic[4])
+                name = (titanic[2])
                 age_name = (age,name)
                 age_list.append(age_name)
 
-
             except ValueError:
                 continue
+
 
             if titanic[3] =="female":
                 total_female_age += age
@@ -56,12 +56,12 @@ for l in u20_woman:
     name = l[1]
     print("%s,%s" % (age, name))
 
-
-
+a = min(age_list)
+b = max(age_list)
 print('女性生存者の平均年齢は%d歳です' %female_ave )
 print('男性生存者の平均年齢は%d歳です' %male_ave )
 print('生存者の最高年齢は%d歳です'%max_age)
 print('生存者の最小年齢は%d歳です'%minimu_age)
 
-print('生存者の最小年齢は%sの%d歳です' %min_age1,name_list2)
-#print('生存者の最高年齢は%sの%d歳です' %age_list[0], max(age_list[1]) )
+print('生存者の最小年齢は%sの%d歳です' %(a[1],a[0]))
+print('生存者の最高年齢は%sの%d歳です' %(b[1],b[0]))
