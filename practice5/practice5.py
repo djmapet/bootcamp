7d6833964af1409981d96d4564e0c0c0e159a62f
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-07 14:13:30 +0900

    「カウント数える」で書き直したのでcommit

--- practice5/practice5.py
+++ practice5/practice5.py
@@ -2,8 +2,8 @@
 import csv
 u20_woman = list()
 age_list = list()
+count_survived = 0
 ave = list()
-survived_count = 0
 total_female_age = 0
 total_male_age = 0
 female_count = 0
@@ -28,8 +28,6 @@ with open("titanic.csv", "r") as open_text:
                 name = (titanic[2])
                 age_name = (age, name)
                 age_list.append(age_name)
-                survived_count += 1
-
             except ValueError:
                 continue
 
@@ -58,15 +56,16 @@ min_age_name = min(age_list)
 (age1, name1) = min_age_name
 max_age_name = max(age_list)
 (age2, name2) = max_age_name
+
 survived_number = age_list
 list.sort(survived_number)
 last_number = survived_number[333]
-(age3, name3) =last_number
+(age3, name3) = last_number
 print('女性生存者の平均年齢は%d歳です' % int(female_ave))
 print('男性生存者の平均年齢は%d歳です' % int(male_ave))
 print('生存者の最高年齢は%d歳です' % int(max_age))
 print('生存者の最小年齢は%d歳です' % (int(mini_age)))
 print('生存者の最小年齢は%sの%d歳です' % (name1, int(age1)))
 print('生存者の最高年齢は%sの%d歳です' % (name2, int(age2)))
-print("生存者は%d名です" % survived_count)
+print("生存者は%d名です" % (len(age_list)))
 print("334人目の生存者は%sの%d歳です" % (name3, int(age3)))
