9272000c0f386c83635fbed642087262e43e5952
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-12 15:17:18 +0900

    print(oldest_male_age)の状態で出力できたのでcommit

--- practice6/practice6.py
+++ practice6/practice6.py
@@ -21,17 +21,13 @@ with open("titanic.csv", "r") as open_text:
             continue
         if titanic[3] == "male":
             try:
-                male_age = int(titanic[4])
-                male_name = (titanic[2])
-                male_age_name = (male_age, male_name)
+                male_age_name = (age, name)
                 male_list.append(male_age_name)
             except ValueError:
                 continue
         if titanic[3] == "female":
             try:
-                female_age = int(titanic[4])
-                female_name = (titanic[2])
-                female_age_name = (female_age,female_name)
+                female_age_name = (age, name)
                 female_list.append(female_age_name)
             except ValueError:
                 continue
@@ -39,13 +35,13 @@ with open("titanic.csv", "r") as open_text:
 #male_list.sort()
 #female_list.sort()
 
-oldest_male_age = max(male_list)
-(male_age) = oldest_male_age
-oldest_female_age = max(female_list)
-(age2) = oldest_female_age
+(age1, name1) = max(male_list)
+oldest_male_age = age1
+(age2, name2) = max(female_list)
+oldest_female_age = age2
 
-print("男性乗客の最高年齢は%d歳です" % oldest_male_age[0])
-print("女性乗客の最高年齢は%d歳です" % oldest_female_age[0])
+print("男性乗客の最高年齢は%d歳です" % oldest_male_age)
+print("女性乗客の最高年齢は%d歳です" % oldest_female_age)
 
 n_gender = 0
 n_gender_cnt = 0
@@ -54,4 +50,3 @@ print("乗客は性別で%sの方が%d人多く乗っていました" % (n_gende
 total_pass_cnt = 0
 dead_cnt = 0
 print("乗客数は%dで、うち死亡したのは%d人です" % (total_pass_cnt, dead_cnt))
-
