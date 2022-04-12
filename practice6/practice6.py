7eef8d384218d5ef29758a32ae7eed0dcf31551f
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-12 17:46:33 +0900

    問題の答えが全て出たのでcommit

--- practice6/practice6.py
+++ practice6/practice6.py
@@ -5,6 +5,9 @@ male_list = list()
 female_list = list()
 oldest_male_age = 0
 oldest_female_age = 0
+dead_cnt = 0
+live_cnt = 0
+error_cnt = 0
 
 with open("titanic.csv", "r") as open_text:
     reader = csv.reader(open_text, delimiter="\t", doublequote=True, lineterminator="\r\n", quotechar='"',
@@ -12,34 +15,45 @@ with open("titanic.csv", "r") as open_text:
     header = next(reader)
 
     for titanic in reader:
+        try:
+            age = float(titanic[4])
+        except ValueError:
+            error_cnt += 1
+            continue
+
         if titanic[3] == "male":
-            try:
-                age = int(titanic[4])
-                male_list.append(age)
-            except ValueError:
-                continue
+            male_list.append(age)
         if titanic[3] == "female":
-            try:
-                age = int(titanic[4])
-                female_list.append(age)
-            except ValueError:
-                continue
+            female_list.append(age)
+        if titanic[0] == "0":
+            dead_cnt += 1
+        if titanic[0] == "1":
+            live_cnt += 1
 
-#male_list.sort()
-#female_list.sort()
+oldest_male_age = max(male_list)
+oldest_female_age = max(female_list)
 
-(age) = max(male_list)
-oldest_male_age = age
-(age) = max(female_list)
-oldest_female_age = age
-
-print("男性乗客の最高年齢は%d歳です" % oldest_male_age)
-print("女性乗客の最高年齢は%d歳です" % oldest_female_age)
+print("男性乗客の最高年齢は%1.1f歳です" % oldest_male_age)
+print("女性乗客の最高年齢は%1.1f歳です" % oldest_female_age)
 
 n_gender = 0
 n_gender_cnt = 0
+male_cnt = len(male_list)
+female_cnt = len(female_list)
+if male_cnt > female_cnt:
+    n_gender = "男性"
+    n_gender_cnt = male_cnt - female_cnt
+if female_cnt > male_cnt:
+    n_gender = "女性"
+    n_gender_cnt = female_cnt - male_cnt
+print("男性が%d、女性が%d" % (male_cnt, female_cnt))
+
 print("乗客は性別で%sの方が%d人多く乗っていました" % (n_gender, n_gender_cnt))
 
-total_pass_cnt = 0
-dead_cnt = 0
+total_pass_cnt = male_cnt + female_cnt
+live_dead = dead_cnt + live_cnt
+
 print("乗客数は%dで、うち死亡したのは%d人です" % (total_pass_cnt, dead_cnt))
+print("生存者は%d人です" % live_cnt)
+print("合計は%d人です" % live_dead)
+print("エラーの数は%d人です" % error_cnt)
