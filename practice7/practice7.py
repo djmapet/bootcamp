ef086d5da1932deca8e3cb59b3a5e620a9a98ea1
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-14 21:17:54 +0900

    女性の多い数を出せたのでcommit

--- practice7/practice7.py
+++ practice7/practice7.py
@@ -1,7 +1,7 @@
 # coding: UTF-8
 #Pclassの1,2,3の数を数える
 #生存と生死とsurevive,pclass,name,sex,ageをリストに取れるように
-
+#1,2,3等客の中で女性が多いのは等はどれか
 import csv
 passenger_list = list()
 male_cnt = 0
@@ -24,16 +24,17 @@ with open("titanic.csv", "r") as open_text:
         pclass = titanic[1]
         name = titanic[2]
         sex = titanic[3]
-        passenger = (survived,pclass, name, sex, age, fare)
+        passenger = (survived, pclass, name, sex, age, fare)
         passenger_list.append(passenger)
 
 p1female = list(filter(lambda x: x[1] == '1' and x[3] == "female", passenger_list))
-age_sum = sum(list(map(lambda x: x[4], p1female)))
-female_cnt = len(list(map(lambda x: x[4], p1female)))
+agelist = list(map(lambda x: x[4], p1female))
+age_sum = sum(agelist)
+female_cnt = len(p1female)
 female_ave = age_sum/female_cnt
 p1male = list(filter(lambda x: x[1] == '1' and x[3] == "male", passenger_list))
 age_sum = sum(list(map(lambda x: x[4], p1male)))
-male_cnt = len(list(map(lambda x: x[4], p1male)))
+male_cnt = len(p1male)
 male_ave = age_sum/male_cnt
 print("１等の平均年齢は、女性が%1.1f歳で男性は%1.1f歳です" %(female_ave, male_ave))
 
@@ -60,3 +61,15 @@ print("2等客の生存率は%1.1fです" %p2_alive_ave)
 p3_alive = len(list(filter(lambda x: x[0] == '1', p3)))
 p3_alive_ave = p3_alive/p3_cnt*100
 print("3等客の生存率は%1.1fです" %p3_alive_ave)
+
+#1,2,3等客の中で女性が多いのは等はどれか
+many_female1 = list(filter(lambda x: x[3] == 'female', p1))
+many_female1_cnt = len(many_female1)
+print("1等客の女性は%d名です" %many_female1_cnt)
+many_female2 = list(filter(lambda x: x[3] == 'female', p2))
+many_female2_cnt = len(many_female2)
+print("2等客の女性は%d名です" %many_female2_cnt)
+many_female3 = list(filter(lambda x: x[3] == 'female', p3))
+many_female3_cnt = len(many_female3)
+print("3等客の女性は%d名です" %many_female3_cnt)
+print('よって女性が多いのは3等客の%d名です' %many_female3_cnt)
