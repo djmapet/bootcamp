a7ed4edeceaf8c5749e21eb7e7ed03e48339dc8e
djmapet <djmapet@gmail.com>
2022-04-20 18:05:44 +0900

    sexとeventを追加

--- practice8/practice8.py
+++ practice8/practice8.py
@@ -1,18 +1,22 @@
 # coding: UTF-8
 import csv
 import collections
+
 player_list = list()
 with open("olympic_tennis.csv", "r") as open_text:
-    reader = csv.reader(open_text, delimiter="\t", doublequote=True, lineterminator="\r\n", quotechar='"',skipinitialspace=True)
+    reader = csv.reader(open_text, delimiter="\t", doublequote=True, lineterminator="\r\n",
+                        quotechar='"', skipinitialspace=True)
     header = next(reader)
 
     for tennis in reader:
+        sex = tennis[2]
         medal = tennis[14]
+        event = tennis[6]
         noc = tennis[7]
-        player = (noc, medal)
+        player = (sex, noc, event, medal)
         player_list.append(player)
 
-
-l = list(filter(lambda x: x[1] == 'Gold' or x[1] == 'Silver' or x[1] == 'Bronz', player_list))
-c = collections.Counter(list(map(lambda x: x[0], l)))
-print(c)
+list_noc_medal = list(map(lambda x: (x[1], x[3]), player_list))
+list_medalist = list(filter(lambda x: x[1] == 'Gold' or x[1] == 'Silver' or x[1] == 'Bronze', list_noc_medal))
+c = collections.Counter(list(map(lambda x: x[0], list_medalist)))
+print(list_medalist[0])
