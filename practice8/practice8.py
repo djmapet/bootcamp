7c34026f939963f6b9d8232091e9c69b5fbaf472
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-04-20 21:25:32 +0900

    cのソートが解らないのでcommit

--- practice8/practice8.py
+++ practice8/practice8.py
@@ -11,12 +11,15 @@ with open("olympic_tennis.csv", "r") as open_text:
     for tennis in reader:
         sex = tennis[2]
         medal = tennis[14]
-        event = tennis[6]
+        event = tennis[13]
         noc = tennis[7]
         player = (sex, noc, event, medal)
         player_list.append(player)
 
-list_noc_medal = list(map(lambda x: (x[1], x[3]), player_list))
-list_medalist = list(filter(lambda x: x[1] == 'Gold' or x[1] == 'Silver' or x[1] == 'Bronze', list_noc_medal))
+list_noc_medal = list(map(lambda x: (x[1], x[3],x[0],x[2]), player_list))
+list_medalist = list(filter(lambda x: (x[1] == 'Gold' or x[1] == 'Silver' or x[1] == 'Bronze') and x[2] == 'M' and x[3] == "Tennis Men's Singles", list_noc_medal))
 c = collections.Counter(list(map(lambda x: x[0], list_medalist)))
-print(list_medalist[0])
+c_sorted = sorted(c,key=lambda x: x,reverse=True)
+print(c)
+print(c_sorted)
+print("メダルを多く取ってる国は%sです"%(c_sorted[0]))
