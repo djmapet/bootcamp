fa6d0abaf3461093e1a48373e6ac3e7252a56943
djmapet <djmapet@gmail.com>
2022-04-28 19:38:15 +0900

    PEP8に準拠

--- practice8/make_db.py
+++ practice8/make_db.py
@@ -4,16 +4,17 @@ import sqlite3
 
 con = sqlite3.connect("tennis.db")
 cur = con.cursor()
-query = "CREATE TABLE IF NOT EXISTS tennis(id,name,sex,age,height,weight,team,noc,games,year,season,city,sport,event,medal)"
+query = "CREATE TABLE IF NOT EXISTS tennis(id,name,sex,age,height,weight,team,noc,games,\
+        year,season,city,sport,event,medal)"
 cur.execute(query)
 with open("olympic_tennis.csv", "r") as open_text:
     reader = csv.reader(open_text, delimiter="\t", doublequote=True, lineterminator="\r\n",
-                    quotechar='"', skipinitialspace=True)
+                        quotechar='"', skipinitialspace=True)
     header = next(reader)
 
     rows = list()
     for row in reader:
-		rows.append(row)
+        rows.append(row)
 
     cur.executemany(
         "INSERT INTO tennis(id,name,sex,age,height,weight,team,noc,games,year,season,city,sport,event,medal) \
