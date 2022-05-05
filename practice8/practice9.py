200f1b694dae075124fcad29ef8589f74d346622
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-05-05 15:39:34 +0900

    SQLで最高齢を出せたのでcommit

--- practice8/practice9.py
+++ practice8/practice9.py
@@ -6,6 +6,10 @@ con = sqlite3.connect("tennis.db")
 cur = con.cursor()
 
 query = "select name, medal from tennis where Medal = 'Gold' AND sex = 'M'"
+query1 = "select max(age) from tennis where Medal = 'Gold' AND sex = 'M' "
 
 for i in cur.execute(query):
 	print("%s %s" % i)
+
+for s in cur.execute(query1):
+	print("%s" % s)
