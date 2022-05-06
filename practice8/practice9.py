dec421132dc31ea9abb3bdf1c193de33252a3d3f
djmapet <djmapet@gmail.com>
2022-05-06 15:37:55 +0900

    フィールドをカラム名でアクセスできるように修正

--- practice8/practice9.py
+++ practice8/practice9.py
@@ -1,8 +1,8 @@
 # coding: UTF-8
-import csv
 import sqlite3
 
 con = sqlite3.connect("tennis.db")
+con.row_factory = sqlite3.Row
 cur = con.cursor()
 
 #query = "select name, medal from tennis where Medal = 'Gold' AND sex = 'M'"
@@ -12,4 +12,4 @@ cur = con.cursor()
 query = "select max(age) as max_age from tennis where Medal = 'Gold' AND sex = 'M' "
 cur.execute(query)
 n = cur.fetchone()
-print("%d" % (n[0]))
+print("%d" % (n['max_age']))
