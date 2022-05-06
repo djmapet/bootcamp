52699a33284c4c8870a6fae2fcd263f0f7f65794
djmapet <djmapet@gmail.com>
2022-05-06 15:11:35 +0900

    SQLを整理

--- practice8/practice9.py
+++ practice8/practice9.py
@@ -5,11 +5,11 @@ import sqlite3
 con = sqlite3.connect("tennis.db")
 cur = con.cursor()
 
-query = "select name, medal from tennis where Medal = 'Gold' AND sex = 'M'"
-query1 = "select max(age) from tennis where Medal = 'Gold' AND sex = 'M' "
+#query = "select name, medal from tennis where Medal = 'Gold' AND sex = 'M'"
+#for i in cur.execute(query):
+#	print("%s %s" % i)
 
-for i in cur.execute(query):
-	print("%s %s" % i)
-
-for s in cur.execute(query1):
-	print("%s" % s)
+query = "select max(age) as max_age from tennis where Medal = 'Gold' AND sex = 'M' "
+cur.execute(query)
+n = cur.fetchone()
+print("%d" % (n[0]))
