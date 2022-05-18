4855343f8dabe65de900ac0e62f608f4274b7ea3
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-05-18 19:06:58 +0900

    havingを付け足したのでcommit

--- practice8/practice9.py
+++ practice8/practice9.py
@@ -12,9 +12,10 @@ cur = con.cursor()
 query = "select max(age) as max_age from tennis where Medal = 'Gold' AND sex = 'M' "
 cur.execute(query)
 n = cur.fetchone()
-print("メダルの個数は%d個です" % (n['max_age']))
+print("メダルの個数は%s個です" % (n['max_age']))
 
-query = "select count(medal) as sum_medal ,name as gold_name from tennis where medal = 'Gold' and sex = 'M' group by gold_name order by sum_medal desc "
+query = "select count(medal) as sum_medal ,name as gold_name from tennis where medal = 'Gold' and sex = 'M' group by gold_name having sum_medal >=4 order by sum_medal desc "
 cur.execute(query)
 m = cur.fetchone()
 print("メダルを多く取った人は%sの%s個です" % (m['gold_name'], m['sum_medal']))
+
