f54ce491ff288c304860f83c81db03706c9a0309
djmapet <djmapet@gmail.com>
2023-01-16 15:42:56 +0900

    dbの作成プログラム追加

---
+++ practice11/make_db.py
@@ -0,0 +1,38 @@
+# coding: UTF-8
+import csv
+import sqlite3
+
+with open("ohtani_pitch_2022.csv", "r") as open_text:
+    reader = csv.reader(open_text, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"',
+                    skipinitialspace=True)
+    header = next(reader)
+
+#print(header)
+#for n in header:
+element = ",".join(header)
+element_cnt = len(header)
+
+scheme = "sample("+element.replace('"','')+")"
+
+l_element = list()
+for n in header:
+    l_element.append("?")
+s_element = ",".join(l_element)
+
+con = sqlite3.connect("sample.db")
+cur = con.cursor()
+query = "CREATE TABLE IF NOT EXISTS "+scheme
+cur.execute(query)
+with open("ohtani_pitch_2022.csv", "r") as open_text:
+    reader = csv.reader(open_text, doublequote=True, lineterminator="\r\n",
+                        quotechar='"', skipinitialspace=True)
+    header = next(reader)
+
+    rows = list()
+    for row in reader:
+        rows.append(row)
+
+    n_query = "INSERT INTO "+scheme+" VALUES ("+s_element+")"
+    print(n_query)
+    cur.executemany(n_query, rows)
+    con.commit()
