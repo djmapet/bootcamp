cbf40e39cbad61c784b49b9694df569ba5ada3da
Ai Tsujikawa <tuji.kamome@gmail.com>
2023-01-29 12:44:08 +0900

    平均スピード def get_speed_ave()の途中部分をコミット

--- practice11/sample.py
+++ practice11/sample.py
@@ -1,5 +1,6 @@
 # coding: UTF-8
 import csv
+import sqlite3
 n_list = list()
 with open("ohtani_pitch_2022.csv", "r") as open_text:
     reader = csv.reader(open_text, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"',
@@ -13,3 +14,25 @@ with open("ohtani_pitch_2022.csv", "r") as open_text:
 
 for n in n_list:
     print("%s,%d" % (n[0],n[1]))
+
+def get_speed_ave(speed_ave):
+    con =sqlite3.connect('sample.db')
+    cur = con.cursor()
+    query = "select avg(release_speed) from sample desc"
+    cur.execute(query)
+    average = cur.fetchone()
+    print("大谷の平均急速は%1.1f mlです") %(average)
+
+
+
+if __name__ == '__main__':
+    get_speed_ave()
+    #for n2 in get_speed_ave():
+     #   print("大谷選手の平均の球速は%1.1f mlです" % float(n2))
+
+
+
+
+
+
+
