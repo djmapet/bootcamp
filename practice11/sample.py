61fc6460852a8156c8fc21a4e67088310ba77f75
Ai Tsujikawa <tuji.kamome@gmail.com>
2023-01-30 22:56:56 +0900

    平均スピードを出力できたのでcommit

--- practice11/sample.py
+++ practice11/sample.py
@@ -1,5 +1,6 @@
 # coding: UTF-8
 import csv
+import sqlite3
 n_list = list()
 with open("ohtani_pitch_2022.csv", "r") as open_text:
     reader = csv.reader(open_text, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"',
@@ -13,3 +14,28 @@ with open("ohtani_pitch_2022.csv", "r") as open_text:
 
 for n in n_list:
     print("%s,%d" % (n[0],n[1]))
+
+def get_speed_ave():
+    con =sqlite3.connect('sample.db')
+    cur = con.cursor()
+    query = "select avg(release_speed) from sample desc"
+    cur.execute(query)
+    info = cur.fetchone()
+    if info:
+        return info[0]
+    else:
+        return None
+
+
+
+
+if __name__ == '__main__':
+    print("大谷の球速平均スピードは%s mphです"%round(get_speed_ave(),1))
+
+
+
+
+
+
+
+
